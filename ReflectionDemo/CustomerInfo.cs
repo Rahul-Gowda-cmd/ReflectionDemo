86a77007230d8be9a6f2b9a9864bcd88ac7fa7b9
HP <rahul.prabu.07@gmail.com>
2021-08-23 23:34:06 +0530

    Add project files.

---
+++ ReflectionDemo/CustomerInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReflectionDemo
+{
+    class CustomerInfo
+    {
+        public int ID { get; set; }
+        public string ClassName { get; set; }
+        public CustomerInfo(int id, string name)
+        {
+            this.ID = id;
+            this.ClassName = name;
+        }
+        public CustomerInfo()
+        {
+            this.ID = -1;
+            this.ClassName = string.Empty;
+        }
+
+        public void PrintId()
+        {
+            Console.WriteLine("Id is {0} :" + this.ID);
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine("Name is {0} " + this.ClassName);
+        }
+    }
+}
